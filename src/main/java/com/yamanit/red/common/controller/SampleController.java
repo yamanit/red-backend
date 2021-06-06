d6bd54f492508dfc43244330a502756213c4e673
scottSeo <seoseok.h@gmail.com>
2021-06-07 00:17:03 +0900

    - update package name - set Swagger 3

--- src/main/java/com/yamanit/red/common/controller/SampleController.java
+++ src/main/java/com/yamanit/red/common/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.yamanit.red.common.contgroller;
+package com.yamanit.red.common.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
